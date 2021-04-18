61e0f02cf156bc4f364b0d5e6f72927896574557
rahul215 <rahul.saxena_cs18@gla.ac.in>
2021-04-18 21:18:56 +0530

    js removed

--- JavaScript/CreateResume.js
+++ JavaScript/CreateResume.js
@@ -1,13 +1,3 @@
 function addNewWEField() {
-    //console.log("Adding New Field");
-
-    let newNode=document.createElement('textarea');
-    newNode.classList.add('form-control');
-    newNode.classList.add('weField');
-    newNode.setAttribute("rows", 3);
-
-    let weOb = document.getElementById("we");
-    let weAddButtonOb = document.getElementById("weAddButton");
-
-    weOb.insertBefore(newNode, weAddButtonOb);
+    
 }
